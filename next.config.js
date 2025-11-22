8a131caf8a0c6785343c28e4609118bbf65ca2e7
Thomas <70901054+ThomasK3@users.noreply.github.com>
2025-11-22 13:12:02 +0100

    Disable TS checks

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      ignoreDuringBuilds: true, // ← PŘIDEJ TOHLE
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true, // ← PŘIDEJ
     },
   };
-  
+    
   module.exports = nextConfig;
